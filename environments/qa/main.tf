6ecd9dea1bc692d57620a5006136c002cd695702
Iam-mithran <martinmithran@gmail.com>
2025-05-28 10:53:30 +0530

    Final Commit

--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -1,4 +1,3 @@
-/*
 module "vpc" {
   source        = "../../modules/vpc"
   vpc_cidr      = "11.0.0.0/16"
@@ -15,4 +14,3 @@ module "ec2" {
   ec2_count         = 3
   env               = "qa"
 }
-*/
